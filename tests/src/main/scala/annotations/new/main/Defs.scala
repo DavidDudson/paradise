2076604266f381826ef8816aaade719ee7c4f2e8
David Dudson <d.dudson@psenterprise.com>
2016-08-11 19:58:49 +1200

    Improve PrintDefn compilation test

--- tests/src/main/scala/annotations/new/main/Defs.scala
+++ tests/src/main/scala/annotations/new/main/Defs.scala
@@ -5,24 +5,42 @@ import scala.meta._
 
 @compileTimeOnly("@printDef not expanded")
 class printDef extends scala.annotation.StaticAnnotation {
-  inline def apply(defn: Any) = meta {
-    assert(defn.is[Defn.Def])
+  inline def apply(defn: Defn.Def) = meta {
     q"println(${defn.toString})"
   }
 }
 
 @compileTimeOnly("@printVal not expanded")
 class printVal extends scala.annotation.StaticAnnotation {
-  inline def apply(defn: Any) = meta {
-    assert(defn.is[Defn.Val])
+  inline def apply(defn: Defn.Val) = meta {
+    q"println(${defn.toString})"
+  }
+}
+
+@compileTimeOnly("@printVar not expanded")
+class printVar extends scala.annotation.StaticAnnotation {
+  inline def apply(defn: Defn.Var) = meta {
     q"println(${defn.toString})"
   }
 }
 
 @compileTimeOnly("@printClass not expanded")
 class printClass extends scala.annotation.StaticAnnotation {
-  inline def apply(defn: Any) = meta {
-    assert(defn.is[Defn.Class])
+  inline def apply(defn: Defn.Class) = meta {
+    q"println(${defn.toString})"
+  }
+}
+
+@compileTimeOnly("@printTrait not expanded")
+class printTrait extends scala.annotation.StaticAnnotation {
+  inline def apply(defn: Defn.Trait) = meta {
+    q"println(${defn.toString})"
+  }
+}
+
+@compileTimeOnly("@printObject not expanded")
+class printObject extends scala.annotation.StaticAnnotation {
+  inline def apply(defn: Defn.Object) = meta {
     q"println(${defn.toString})"
   }
 }
--- tests/src/test/scala/annotations/new/main/PrintDefn.scala
+++ tests/src/test/scala/annotations/new/main/PrintDefn.scala
@@ -1,9 +1,21 @@
 package main
 
-object PrintDefn extends App {
+object PrintDefn {
   @printDef
   def Def = ???
 
+  @printVar
+  var Var = ???
+
   @printVal
   val Val = ???
+
+  @printObject
+  object Object
+
+  @printTrait
+  trait Trait
+
+  @printClass
+  class Class
 }
